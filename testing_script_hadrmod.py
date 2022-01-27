dc22254b7fb25cd6f5aebb7be7944072264ba34d
micro-uhecr <leonel.morejon@uni-wuppertal.de>
2022-01-27 01:31:44 +0100

    Updated testing script to include random seeds

--- testing_script_hadrmod.py
+++ testing_script_hadrmod.py
@@ -5,7 +5,7 @@ sys.path.append('CRPropa_works')  # location of hadronic module
 
 from hadronic_module import * 
 
-def TestRun1D(N=100, filename='Test1.txt', density=1e13):
+def TestRun1D(N=100, filename='Test1.txt', density=1e13, seed=None):
     """Testing function for hadronic module in 1D
 
     Primary protons of 1 EeV, one directionalin 1 pc spherical volume.
@@ -26,7 +26,7 @@ def TestRun1D(N=100, filename='Test1.txt', density=1e13):
     sim.add(det)
 
     module1 = SimplePropagation(0.01*pc, 0.5*pc)
-    module3 = HadronicInteractions(matter_density=density)
+    module3 = HadronicInteractions(matter_density=density, seed=seed)
     sim.add(module1)
     sim.add(module3)
     sim.add(SphericalBoundary(Vector3d(0, 0, 0), 1.01 * pc))
@@ -50,4 +50,5 @@ if __name__ == "__main__":
     Nprimaries = int(sys.argv[1])
     output_filename = sys.argv[2]
 
-    TestRun1D(Nprimaries, output_filename)
+    Random_seedThreads(1987)
+    TestRun1D(Nprimaries, output_filename, seed=1967)
