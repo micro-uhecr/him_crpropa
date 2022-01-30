ea03fd95b7bdb7a87b1898c87ac5852f71c63710
micro-uhecr <leonel.morejon@uni-wuppertal.de>
2022-01-30 23:40:11 +0100

    added configurations for primary E, id & daughters

--- config_file.py
+++ config_file.py
@@ -12,4 +12,25 @@ mtag = 'SIBYLL23D'
 
 impy_directory = '/home/leonel/GitProjects/impy/'
 
+# TODO: Take this from impy config, for the respective HImodel 
+# Minimal ecm to model hadronic interaction
+Ecm_min = 10 # GeV 
+
+# Minimal energy for secondaries produced in hadronic interaction
 Emin = 10 # GeV
+
+# List of primaries that can undergo hadronic interaction
+allowed_primaries = [
+    1000010010, # protons only
+    # -1000010010, 1000010010, # (anti)protons
+    # -1000000010, 1000000010, # (anti)neutrons
+]
+
+# List of secondaries produced in hadronic interaction, due to impy not using stable to limit secondaries
+allowed_secondaries = [
+    22,               # gamma  
+    11, -11,          # electron, positron
+    211, 111, -211,   # pions
+    -2112, 2112,      # (anti)neutron
+    -2212, 2212,      # (anti)proton
+]
