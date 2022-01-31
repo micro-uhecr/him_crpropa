b3c3a2a4f9784a4cf880fc8044f94ceb61e723f7
micro-uhecr <leonel.morejon@uni-wuppertal.de>
2022-01-31 12:14:18 +0100

    this one works w previous version profile_time

--- testing_script_hadrmod.py
+++ testing_script_hadrmod.py
@@ -45,10 +45,57 @@ def TestRun1D(N=100, filename='Test1.txt', density=1e13, seed=None):
     sim.run(source, N)
     
     
+def TestRun1D_tracks(N=100, filename='Test1.txt', density=1e13, seed=None):
+    """Testing function for hadronic module in 1D
+
+    Primary protons of 1 EeV, one directionalin 1 pc spherical volume.
+    Cutoff energy is 1 TeV, only hadronic interactions for protons with
+    Cross section 10mb
+
+    """
+
+    #specifying the output
+    particle_positions = TextOutput(filename, Output.Trajectory3D)
+    
+    # Detection grid
+
+    sim = ModuleList()
+    det = Observer()
+    # det.add(ObserverTracking(Vector3d(0,0,0), pc, 0.01*pc))
+    det.add(ObserverSurface(Sphere(Vector3d(0,0,0), 1 * pc)))
+    det.onDetection(particle_positions)
+    sim.add(det)
+        
+    module1 = SimplePropagation(0.3*pc, 1.5*pc)
+    if seed == None:
+        module3 = HadronicInteractions(matter_density=density)
+    else:
+        module3 = HadronicInteractions(matter_density=density, seed=seed)
+
+    sim.add(module1)
+    sim.add(module3)
+    sim.add(SphericalBoundary(Vector3d(0, 0, 0), 1.01 * pc))
+    
+    sim.add( MinimumEnergy( 10 * GeV) )    
+    
+    # Specifying the source
+    source = Source()
+    source.add(SourceParticleType(nucleusId(1, 1)))
+    source.add(SourceDirection(Vector3d(0, 0, 1)))
+    source.add(SourcePosition(0))
+    source.add(SourceEnergy(1 * EeV))
+
+    # Run particle
+    sim.setShowProgress(True)
+    sim.run(source, N)
+
 if __name__ == "__main__":
     # TestRun1D(100000, 'output.txt')
     Nprimaries = int(sys.argv[1])
     output_filename = sys.argv[2]
 
     Random_seedThreads(1987)
-    TestRun1D(Nprimaries, output_filename, seed=1967)
+    # TestRun1D(Nprimaries, output_filename, seed=1967)
+    TestRun1D_tracks(Nprimaries, output_filename, seed=1967, density=1e17)
+
+    # print(Random_seedThreads.getSeed())
