e95bfac5c1a5074690aec6ecb82360b80786b882
mohller <leonel.rmh@gmail.com>
2023-09-12 15:03:49 +0200

    renaming package

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 from hadronic_module import __version__
 
 setup(
-    name='hadronic_module',
+    name='him_crpropa',
     version=__version__,
 
     url='https://github.com/micro-uhecr/prototype_him_crpropa',
     author='Leonel Morejon',
     author_email='leonel.morejon@uni-wuppertal.de',
 
-    py_modules=['hadronic_module'],
+    py_modules=['him_crpropa'],
 )
